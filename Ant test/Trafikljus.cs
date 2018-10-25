941862c28f4e4568ad91867cb15183b9bd2f5053
Bolger Lightfoot <BolgerLightfoot@outlook.com>
2018-10-25 12:16:39 +0200

    Updatering i trafikljus.cs

--- Ant test/Trafikljus.cs
+++ Ant test/Trafikljus.cs
@@ -21,11 +21,12 @@ namespace Ant_test
         /// <param name="pos">Position</param>
         /// <param name="dir">Riktning 0-3</param>
         /// <param name="color">Färg</param>
-        public Trafikljus(Point pos, int dir,  Color color)
+        public Trafikljus(Point pos, int dir,  Color color,int v_max)
         {
             this.pos = pos;
            this.color = color;
             this.dir = dir;
+            Point[,] rutor = new Point[1,(int) Math.Pow(v_max, 2) * v_max / 2];
         }
         /// <summary>
         ///  Inizialerar en ny trafikljus
@@ -34,7 +35,7 @@ namespace Ant_test
         /// <param name="Y">Y postition</param>
         /// <param name="dir">Riktning g0-3</param>
         /// <param name="color">Färg</param>
-        public Trafikljus(int X, int Y, int dir, Color color)
+        public Trafikljus(int X, int Y, int dir, Color color, int v_max)
         {
             pos = new Point(X, Y);
             this.color = color;
@@ -43,20 +44,9 @@ namespace Ant_test
 
         public void Rödljus() // Metoden kräver kod från andra delar av programmet.
         {
-            int[] redlights = new int[2];
+            
 
-            for (int i=0; i<Math.Pow(v_max,2)*v_max/2; i++)
-            {
-                switch (dir)
-                {
-                    case 0: break;
-                    redlights.SetValue
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
-                }
-                
-            }
+            
             
 
                  
