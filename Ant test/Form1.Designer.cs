10950fb2952d3e7c606aabc6a6b11580c0ca45f7
Filip Elihn <Folip2@gmail.com>
2019-02-14 02:29:28 +0100

    Tog bort kanterna

--- Ant test/Form1.Designer.cs
+++ Ant test/Form1.Designer.cs
@@ -64,7 +64,6 @@
             this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox.Location = new System.Drawing.Point(12, 12);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(365, 314);
